71698ec0a3b8705239651b6a3a9b5b68b5ff5ae2
Shingo Omura <everpeace@gmail.com>
2016-08-22 15:17:21 +0900

    fix bump-version.sh

--- ci/tasks/bump-version.sh
+++ ci/tasks/bump-version.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
 VERSION=`cat version/number`-SNAPSHOT
-MESSAGE="[ci skip][Concourse CI] Bump Version ($VERSION)"
+MESSAGE="[ci skip][Concourse CI] Bump Version (${VERSION})"
+VERSION_FILE="version.sbt"
 
 cd out
 shopt -s dotglob
 mv -f ../repo/* ./
 
-echo "Bump to $VERSION"
-sed -i.bak -e "s/version := \".*\"/version := \"$VERSION\"/" build.sbt
-rm build.sbt.bak
+echo "Bump to ${VERSION}"
+sed -i.bak -e "s/version := \".*\"/version := \"${VERSION}\"/" "${VERSION_FILE}"
+rm "${VERSION_FILE}".bak
 
 git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_NAME}"
-git add build.sbt
+git add "${VERSION_FILE}"
 git commit -m "${MESSAGE}"
